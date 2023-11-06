f8fdb24224c82ae9e3eac7a5d438286b9e9c4b9b
Alessia Pellicoro <pellicoroalessia@gmail.com>
2023-11-06 16:41:22 +0100

    :sparkles: implementation mailJS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log("Hello")
+/**
+ * Email.js 
+ * 
+ */
+// init
+(function(){
+    emailjs.init(ApiKeyMailJS);
+})();
+// email js send mail
+function sendMail(){
+    var params = {
+        from_name : document.getElementById("fullName").value,
+        email_id : document.getElementById("email_id").value,
+        object: document.getElementById("object").value,
+        message : document.getElementById("message").value
+    }
+    emailjs.send("service_kxs2r8l" , "template_6i7uo28", params).then(function (res){
+        alert("Success!" + res.status);
+    })
+}
