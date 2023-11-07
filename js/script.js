3797ddc6ef8c807abd9f780e54590c4e25dc3e8a
Alessia Pellicoro <pellicoroalessia@gmail.com>
2023-11-07 15:57:56 +0100

    :sparkles: validation form & pop up mail send

--- js/script.js
+++ js/script.js
@@ -8,13 +8,66 @@
 })();
 // email js send mail
 function sendMail(){
+
+    var formValid = true;
+
     var params = {
-        from_name : document.getElementById("fullName").value,
+        form_name : document.getElementById("fullName").value,
         email_id : document.getElementById("email_id").value,
         object: document.getElementById("object").value,
         message : document.getElementById("message").value
     }
-    emailjs.send("service_kxs2r8l" , "template_6i7uo28", params).then(function (res){
-        alert("Success!" + res.status);
-    })
+
+    // Validation Name
+    if(params.form_name === ''){
+        formValid = false;
+        document.getElementById("fullName").classList.add("error")
+         
+    }
+
+    // Validation Mail
+    if(params.email_id === '' || !params.email_id.includes('@') || !params.email_id.includes('.')){
+        formValid = false
+        document.getElementById("email_id").classList.add("error")
+         
+    }
+
+    // Validation Object
+    if(params.object === ''){
+        formValid = false
+        document.getElementById("object").classList.add("error")
+         
+    }
+
+    // Validation Message
+    if(params.message === ''){
+        formValid = false
+        document.getElementById("message").classList.add("error")
+         
+    }
+
+    if(formValid){
+        // On submit imput return valid 
+        document.getElementById("fullName").classList.remove("error")
+        document.getElementById("email_id").classList.remove("error")
+        document.getElementById("object").classList.remove("error")
+        document.getElementById("message").classList.remove("error")
+
+        // Email send
+        emailjs.send("service_kxs2r8l" , "template_6i7uo28", params).then(function (res){
+            document.getElementById("popup").classList.add("show")
+        })
+    } else {
+        return
+    }
+}
+
+// Close Pop Up
+function closePopup (){
+    document.getElementById("popup").classList.remove("show")
+
+    document.getElementById("fullName").value = ""
+    document.getElementById("email_id").value = ""
+    document.getElementById("object").value = ""
+    document.getElementById("message").value = ""
 }
